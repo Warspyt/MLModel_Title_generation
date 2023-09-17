9275e9928855c9be1ab8fdce101c4d299d32b3ac
Warspyt <tatianax2442@gmail.com>
2023-09-17 16:29:28 -0500

    Librerias

--- TitleGeneratorModel.py
+++ TitleGeneratorModel.py
@@ -1,16 +1,8 @@
-# Librerias a utilizar
-import pandas as pd;
-import string;
-import numpy as np;
-import json;
-from keras.preprocessing.sequence import pad_sequences;
-from keras.layers import Embedding, LSTM, Dense, Dropout;
-from keras.preprocessing.text import Tokenizer;
-from keras.callbacks import EarlyStopping;
-from keras.models import Sequential;
-import keras.utils as ku;
 import tensorflow as tf;
-tf.random.set_seed(2);
-from numpy.random import seed;
-seed(1);
+import pandas as pd;
+import numpy as np;
+import os;
+import time;
 
+# Para utilizar Tensor.numpy() más adelante
+tf.enable_eager_execution();
