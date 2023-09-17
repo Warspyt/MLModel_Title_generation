d8ac544d84d908f7e92ec34f6e6877c3260a2f3f
Warspyt <tatianax2442@gmail.com>
2023-09-17 16:32:06 -0500

    Dataset

--- TitleGeneratorModel.py
+++ TitleGeneratorModel.py
@@ -6,3 +6,11 @@ import time;
 
 # Para utilizar Tensor.numpy() más adelante
 tf.enable_eager_execution();
+
+# Lectura del dataset para el modelo
+data = pd.read_csv('arxiv_data.csv')
+data = data.drop(columns=['summaries','terms'])
+data = data.sample(frac=1)
+data = data[0:500]
+data.head()
+
