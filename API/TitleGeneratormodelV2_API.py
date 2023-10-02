3ac3c6b778648b41fd63a045964e8cf0ba956c7a
Warspyt <tatianax2442@gmail.com>
2023-10-01 21:15:03 -0500

    model ok

--- API/TitleGeneratormodelV2_API.py
+++ API/TitleGeneratormodelV2_API.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/title-generator/{summarize}")
 
-def generate_Title(summarize: str) -> str:
+def generate_Title(summarize: str) -> list:
     
     try:
         # Cargar el modelo entrenado
@@ -16,12 +16,9 @@ def generate_Title(summarize: str) -> str:
         
         options = model.predict(summarize)
         titles = options[0].split(".")
-        titlesValid = [i for i in titles if (i != "" and len(i) > 10)]
-        print(titlesValid)
-        titleSelect = r.randint(0, len(titlesValid) - 1)
-        newsTitle = titlesValid[titleSelect]
+        newsTitle = [i for i in titles if (i != "" and len(i) > 10)]
         
     except:
-        newsTitle = "Error: An exception occurred"
+        newsTitle = ["Error: An exception occurred"]
     
     return newsTitle
